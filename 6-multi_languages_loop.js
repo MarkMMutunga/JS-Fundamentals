50e945857df17e19fb2bb0cb25fa0174e9866be5
Mark_Mutunga <markmiki03@gmail.com>
2025-06-06 17:15:07 +0300

    EDITTT

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -2,10 +2,14 @@
 
 const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let output = '';
-for (let i = 0; i < messages.length; i++) {
+let i = 0;
+
+while (i < messages.length) {
   output += messages[i];
   if (i < messages.length - 1) {
     output += '\n';
   }
+  i++;
 }
+
 console.log(output);
