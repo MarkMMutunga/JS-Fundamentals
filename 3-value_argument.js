9db8e1cd23c705ab95a01b6205d101c824658e0b
Mark_Mutunga <markmiki03@gmail.com>
2025-06-06 14:08:07 +0300

    forth file

---
+++ 3-value_argument.js
@@ -0,0 +1,8 @@
+let chips = process . argv.slice(2);
+if (!chips[0]) {
+    console.log("No argument");
+} else if (!chips[1]) {
+    console.log("Argument found");
+} else {
+    console.log("Arguments found");
+}
