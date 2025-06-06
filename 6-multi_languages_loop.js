5f708180aeb6a26a642ffa5cc606e4accd1bc7a9
Mark_Mutunga <markmiki03@gmail.com>
2025-06-06 17:04:46 +0300

    EDITTT

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 
-const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (const line of lines) {
-  console.log(line);
+const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let output = '';
+for (let i = 0; i < messages.length; i++) {
+  output += messages[i];
+  if (i < messages.length - 1) {
+    output += '\n';
+  }
 }
+console.log(output);
