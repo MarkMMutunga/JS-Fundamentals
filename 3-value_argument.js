70d02b3969df94fb181fda96aaf354666071dee9
Mark_Mutunga <markmiki03@gmail.com>
2025-06-06 14:32:51 +0300

    EDITTT

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,6 +1,8 @@
 let chips = process . argv.slice(2);
-if (!chips[1]) {
+if (!chips[0]) {
+    console.log("No argument");
+} else if (!chips[1]) {
     console.log("HBTN");
-} else if (!chips[2]) {
-    console.log("HBTN cool");
-} 
+} else {
+    console.log("Arguments found");
+}
